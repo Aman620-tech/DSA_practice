9ea8c6973b2e6e3b204c8b5cf291caefd69f8e43
Baa-Aman <pandeyaman997@gmail.com>
2026-05-14 00:05:26 +0530

    loop  practive

---
+++ index6.js
@@ -0,0 +1,49 @@
+// loop  inside loop 
+//  ww qqqq
+
+for (let i = 0; i < 3; i++) {
+      for (let j = i; j >=0; j--) {
+        console.log("i=" + i, "j=" +j)     
+    }
+}
+// for (let i = 0; i < 3; i++) {
+//     // const element = array[i];
+//     // console.log("@")
+//     for (let j = i; j >0; j--) {
+//         console.log("i=" + i, "j=" +j)
+//         // const element = array[j];
+        
+//     }
+    
+// }
+// for (let i = 0; i < 3; i++) {
+//     // const element = array[i];
+//     // console.log("@")
+//     for (let j = 0; j <= i; j++) {
+//         console.log("i=" + i, "j=" +j)
+//         // const element = array[j];
+        
+//     }
+    
+// }
+// for (let i = 0; i < 3; i++) {
+//     // const element = array[i];
+//     // console.log("@")
+//     for (let j = 0; j < i; j++) {
+//         console.log("i=" + i, "j=" +j)
+//         // const element = array[j];
+        
+//     }
+    
+// }
+
+// for (let i = 0; i < 3; i++) {
+//     // const element = array[i];
+//     // console.log("@")
+//     for (let j = 0; j < 3; j++) {
+//         console.log("i=" + i, "j=" +j)
+//         // const element = array[j];
+        
+//     }
+    
+// }
